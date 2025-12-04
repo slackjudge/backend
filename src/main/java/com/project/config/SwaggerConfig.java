4992a95a93390b2cb340f31322ede575578f55f3
백승우 <swthewhite@gmail.com>
2025-12-04 13:48:35 +0900

    feat: implement API info endpoint

--- src/main/java/com/project/config/SwaggerConfig.java
+++ src/main/java/com/project/config/SwaggerConfig.java
@@ -2,18 +2,25 @@ package com.project.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${project.name:Backend API}")
+    private String projectName;
+
+    @Value("${project.version:1.0.0}")
+    private String projectVersion;
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Backend API")
-                        .description("Backend API Documentation")
-                        .version("v1.0.0"));
+                        .title(projectName)
+                        .description(projectName + " Documentation")
+                        .version(projectVersion));
     }
 }
