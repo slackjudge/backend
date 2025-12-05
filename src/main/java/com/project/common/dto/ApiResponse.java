dc434c7792c6797a113d2094164a79722e0a1f9c
arkchive <kyungminkim7@konkuk.ac.kr>
2025-12-05 11:35:35 +0900

    feat/#1: DTO Validation 예외 처리 추가

--- src/main/java/com/project/common/dto/ApiResponse.java
+++ src/main/java/com/project/common/dto/ApiResponse.java
@@ -26,4 +26,8 @@ public class ApiResponse<T> {
     public static <T> ApiResponse<T> error(ErrorCode errorCode) {
         return new ApiResponse<>(false, errorCode.getCode(), errorCode.getMessage(), null);
     }
+
+    public static <T> ApiResponse<T> error(ErrorCode errorCode, String message) {
+        return new ApiResponse<>(false, errorCode.getCode(), message, null);
+    }
 }
--- src/main/java/com/project/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/common/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.project.common.exception;
 
 import com.project.common.dto.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -19,5 +21,22 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
         return ResponseEntity
                 .ok(ApiResponse.error(ErrorCode.INTERNAL_SERVER_ERROR));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Void>> handleValidationException(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .findFirst()
+                .map(FieldError::getDefaultMessage)
+                .orElse(ErrorCode.INVALID_INPUT.getMessage());
+
+        return ResponseEntity.ok(
+                ApiResponse.error(
+                        ErrorCode.INVALID_INPUT,
+                        message
+                )
+        );
     }
 }
