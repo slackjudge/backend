2110b4d9b229eff9ef405160a11995a8738c897f
andrew <leegd120@gmail.com>
2025-12-07 21:46:10 +0900

    feat/#6: AccessToken 및 RefreshToken 로직 구현 및 RefreshToken Repository 정의

---
+++ src/main/java/com/project/service/TokenService.java
@@ -0,0 +1,56 @@
+package com.project.service;
+
+import com.project.dto.response.LoginResponse;
+import com.project.security.jwt.JwtClaims;
+import com.project.security.jwt.JwtProvider;
+import com.project.security.jwt.access.AccessTokenClaim;
+import com.project.security.jwt.refresh.RefreshTokenClaim;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+@Service
+@RequiredArgsConstructor
+public class TokenService {
+
+    private final JwtProvider accessTokenProvider;
+    private final JwtProvider refreshTokenProvider;
+    private final RefreshTokenService refreshTokenService;
+
+
+    public LoginResponse issueTokens(Long userId) {
+        String accessToken = accessTokenProvider.createToken(AccessTokenClaim.of(userId));
+        String refreshToken = refreshTokenProvider.createToken(RefreshTokenClaim.of(userId));
+
+        refreshTokenService.saveRefreshToken(userId, refreshToken);
+        return new LoginResponse(accessToken, refreshToken);
+    }
+
+
+    public LoginResponse reissueToken(String oldRefreshToken) {
+        Long userId = resolveRefreshToken(oldRefreshToken);
+        refreshTokenService.validateRefreshToken(userId, oldRefreshToken);
+        return issueTokens(userId);
+    }
+
+
+    public void logout(Long userId, String refreshToken) {
+
+        refreshTokenService.validateRefreshToken(userId, refreshToken);
+        refreshTokenService.removeRefreshToken(userId);
+    }
+
+    private Long resolveRefreshToken(String refreshToken) {
+        JwtClaims claims = refreshTokenProvider.parseJwtClaimsFromToken(refreshToken);
+        return getClaimValue(claims, "id", Long::parseLong);
+    }
+
+    private static <T> T getClaimValue(JwtClaims claims, String key, Function<String, T> converter) {
+        Object value = claims.getClaims().get(key);
+        if (value != null) {
+            return converter.apply((String) value);
+        }
+        return null;
+    }
+}
