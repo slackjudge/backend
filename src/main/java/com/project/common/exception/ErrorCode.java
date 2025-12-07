509b0c9ecf271587be076dad5c956f9c8793b08e
andrew <leegd120@gmail.com>
2025-12-07 19:03:26 +0900

    feat/#6: 인증/인가 예외처리 관련 에러코드 추가

--- src/main/java/com/project/common/exception/ErrorCode.java
+++ src/main/java/com/project/common/exception/ErrorCode.java
@@ -12,9 +12,24 @@ public enum ErrorCode {
     RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "C002", "Resource not found"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "C003", "Internal server error"),
 
+    // Users
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USERS_001", "User not found"),
+
     // Auth
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "A001", "Unauthorized"),
-    FORBIDDEN(HttpStatus.FORBIDDEN, "A002", "Access denied");
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "AUTH_001", "Unauthorized"),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "AUTH_002", "Access denied"),
+
+    // Jwt
+    EMPTY_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_003", "Access Token is empty"),
+    EXPIRED_ACCESS_TOKEN(HttpStatus.FORBIDDEN, "AUTH_004", "Access Token is expired"),
+    EXPIRED_REFRESH_TOKEN(HttpStatus.FORBIDDEN, "AUTH_005", "Refresh Token is expired"),
+    REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "AUTH_006", "Refresh Token not found"),
+    REFRESH_TOKEN_MISMATCH(HttpStatus.UNAUTHORIZED, "AUTH_007", "RefreshToken is mismatch"),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_008", "Token is invalid"),
+    MALFORMED_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_009", "Token is abnormal"),
+    SIGNATURE_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_010", "Token is manipulated"),
+    UNSUPPORTED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_011", "Token is unsupported"),
+    ;
 
     private final HttpStatus status;
     private final String code;
