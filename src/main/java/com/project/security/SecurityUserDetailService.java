0a3b338bf32f882e88d5bf76ea6757e4c1c1f87f
andrew <leegd120@gmail.com>
2025-12-07 18:45:11 +0900

    feat/#6: JWT 인증용 UserDetails 및 UserDetailsService 구현

---
+++ src/main/java/com/project/security/SecurityUserDetailService.java
@@ -0,0 +1,23 @@
+package com.project.security;
+
+import com.project.entity.UserEntity;
+import com.project.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SecurityUserDetailService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userService.findUser(Long.parseLong(username));
+
+        return new SecurityUserDetails(user);
+    }
+}
