cb63dc34b66c9f9f78b4d29b71c2305f3d42f009
andrew <leegd120@gmail.com>
2025-12-07 18:56:01 +0900

    feat/#6: 인증/인가 예외 핸들러 적용 및 공통 에러 응답 구조 정비

---
+++ src/main/java/com/project/config/security/SecurityAuthConfig.java
@@ -0,0 +1,33 @@
+package com.project.config.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.security.handler.JwtAccessDeniedHandler;
+import com.project.security.handler.JwtAuthenticationEntryPoint;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+@Configuration
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class SecurityAuthConfig {
+
+    @Bean
+    public PasswordEncoder bcryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler(ObjectMapper objectMapper) {
+        return new JwtAccessDeniedHandler(objectMapper);
+    }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return new JwtAuthenticationEntryPoint();
+    }
+}
