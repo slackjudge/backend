5b9b01418bf7a2df1d459693705a2801bfbb3a3a
marulog <gr214777@naver.com>
2025-12-09 09:46:46 +0900

    feat/#14: "유저-문제 연관 관계 매핑"

---
+++ src/main/java/com/project/entity/UsersProblemEntity.java
@@ -0,0 +1,42 @@
+package com.project.entity;
+
+import com.project.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Table(name="user_problem")
+@Getter
+@ToString
+@Entity
+public class UsersProblemEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="users_problem_id")
+    private Long usersProblemId;
+
+
+    /**
+     * 문제 - FK
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="problem_id", nullable = false)
+    private ProblemEntity problem;
+
+    /**
+     * 유저 - FK
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id", nullable = false)
+    private UserEntity user;
+
+    @Column(name="is_solved", nullable = false)
+    private boolean isSolved;
+
+    @Column(name="solved_time")
+    private LocalDateTime solvedTime;
+}
