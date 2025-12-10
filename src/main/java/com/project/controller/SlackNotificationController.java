c7a764b548009bc13094dd4691e7526376924937
arkchive <kyungminkim7@konkuk.ac.kr>
2025-12-10 20:57:22 +0900

    fix/#18: 와일드 카드 제거, 클래스 명시적 임포트

--- src/main/java/com/project/controller/SlackNotificationController.java
+++ src/main/java/com/project/controller/SlackNotificationController.java
@@ -4,7 +4,10 @@ import com.project.common.dto.ApiResponse;
 import com.project.service.SlackNotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequiredArgsConstructor
