bc8d14a0c86041d70533ea166cc0fa5ff910bb53
andrew <leegd120@gmail.com>
2025-12-10 11:17:50 +0900

    fix/#24: api 엔드포인트 /api 제거

--- src/main/java/com/project/common/util/WebSecurityUrl.java
+++ src/main/java/com/project/common/util/WebSecurityUrl.java
@@ -8,8 +8,8 @@ public class WebSecurityUrl {
 
     public static final String[] HEALTH_CHECK_ENDPOINT = {"/health"};
     public static final String[] READ_ONLY_PUBLIC_ENDPOINTS = {"/favicon.ico"};
-    public static final String LOGIN_ENDPOINT = "/api/oauth/login/**";
-    public static final String REISSUE_ENDPOINT = "/api/oauth/reissue";
+    public static final String LOGIN_ENDPOINT = "/oauth/login/**";
+    public static final String REISSUE_ENDPOINT = "/oauth/reissue/**";
 
-    public static final String[] ANONYMOUS_ENDPOINTS = {LOGIN_ENDPOINT};
+    public static final String[] ANONYMOUS_ENDPOINTS = {LOGIN_ENDPOINT, REISSUE_ENDPOINT};
 }
