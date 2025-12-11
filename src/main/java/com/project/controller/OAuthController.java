ec6e43fda2998c19fcb436d1844a187c5b7d471e
andrew <leegd120@gmail.com>
2025-12-11 16:02:54 +0900

    fix/#24: reissue Post로 변경

--- src/main/java/com/project/controller/OAuthController.java
+++ src/main/java/com/project/controller/OAuthController.java
@@ -2,7 +2,6 @@ package com.project.controller;
 
 import com.project.common.dto.ApiResponse;
 import com.project.common.security.SecurityUserDetails;
-import com.project.dto.request.LogoutRequest;
 import com.project.dto.response.LoginResponse;
 import com.project.service.OAuthService;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestHeader;
 
@@ -29,15 +27,14 @@ public class OAuthController {
         return ResponseEntity.ok(ApiResponse.success("Slack 소셜 로그인 성공", oAuthService.slackLogin(code)));
     }
 
-    @GetMapping("/reissue")
+    @PostMapping("/reissue")
     public ResponseEntity<ApiResponse<LoginResponse>> reissue(@RequestHeader(value = "refreshToken") String refreshToken) {
         return ResponseEntity.ok(ApiResponse.success("토큰 재발급 성공", oAuthService.reissueToken(refreshToken)));
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<ApiResponse<Void>> logout(@AuthenticationPrincipal SecurityUserDetails userInfo,
-                                       @RequestBody LogoutRequest logoutRequest) {
-        oAuthService.logout(userInfo.getId(), logoutRequest);
+    public ResponseEntity<ApiResponse<Void>> logout(@AuthenticationPrincipal SecurityUserDetails userInfo) {
+        oAuthService.logout(userInfo.getId());
         return ResponseEntity.ok(ApiResponse.success("로그아웃 성공", null));
     }
 }
