b04fdbb58854c4ee50cce3128d75355099168101
rettooo <gkdud9236@naver.com>
2025-12-12 23:54:04 +0900

    feat/#30: 마이페이지 응답 DTO 설계

--- src/main/java/com/project/dto/response/MyPageResponse.java
+++ src/main/java/com/project/dto/response/MyPageResponse.java
@@ -1,6 +1,9 @@
 package com.project.dto.response;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Getter
@@ -9,7 +12,7 @@ public class MyPageResponse {
     // 사용자 기본 정보
     private Profile profile;
     private List<Grass> grass;
-    private SelectedDateDetail selectedDateDetailDto;
+    private SelectedDateDetail selectedDateDetail;
 
     @Getter
     @Builder
@@ -22,24 +25,30 @@ public class MyPageResponse {
 
     @Getter
     @Builder
+    @AllArgsConstructor // [추가]
+    @NoArgsConstructor  // [추가]
     public static class Grass{
         private String date; //"2021-09-01"
         private int solvedCount;
     }
     @Getter
     @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
     public static class SelectedDateDetail{
         private String date;
         private int dailyScore;
         private int dailyRank;
         private int solvedCount;
         private int maxDifficulty;
-        private List<ProblemDto> problems;
+        private List<Problem> problems;
     }
 
     @Getter
     @Builder
-    public static class ProblemDto{
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Problem{
         private String title;
         private int tierLevel;
         private String link;
