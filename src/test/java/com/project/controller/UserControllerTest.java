5aa1fc3c1f43363c09afdf4fd47aa9d745d749e0
rettooo <gkdud9236@naver.com>
2025-12-12 23:59:07 +0900

    feat/#30: 마이페이지 API 계층 테스트 (WebMvcTest)

--- src/test/java/com/project/controller/UserControllerTest.java
+++ src/test/java/com/project/controller/UserControllerTest.java
@@ -7,10 +7,12 @@ import com.project.common.security.jwt.refresh.RefreshTokenProvider;
 import com.project.common.util.BojUtil;
 import com.project.common.util.SlackUtil;
 import com.project.dto.request.SignUpRequest;
+import com.project.dto.response.MyPageResponse;
 import com.project.entity.EurekaTeamName;
 import com.project.entity.UserEntity;
 import com.project.repository.RefreshTokenRepository;
 import com.project.repository.UserRepository;
+import com.project.service.MyPageService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,7 +28,11 @@ import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -54,6 +60,10 @@ class UserControllerTest {
     RefreshTokenRepository refreshTokenRepository;
     @MockitoBean
     BojUtil bojUtil;
+
+    @MockitoBean
+    MyPageService myPageService;
+
     @Autowired
     MockMvc mockMvc;
     @Autowired
@@ -117,5 +127,56 @@ class UserControllerTest {
         assertThat(updatedUser.getUsername()).isEqualTo("이규동");
         assertThat(updatedUser.getTeamName()).isEqualTo(EurekaTeamName.BACKEND_FACE);
         assertThat(updatedUser.getBojTier()).isEqualTo(14);
+    }
+
+    @Test
+    @DisplayName("마이페이지 조회 성공 테스트")
+    void getMyPage() throws Exception {
+        //given
+        int year = 2025;
+        int month = 12;
+        String date = "2025-12-11";
+
+        //가짜 데이터 생성
+        MyPageResponse mockResponse = MyPageResponse.builder()
+                .profile(MyPageResponse.Profile.builder()
+                        .username("이규동")
+                        .baekjoonId("dlrbehd120")
+                        .tierLevel(14)
+                        .totalScore(1000L)
+                        .build())
+                .grass(Collections.emptyList())
+                .selectedDateDetail(MyPageResponse.SelectedDateDetail.builder()
+                        .date(date)
+                        .dailyScore(20)
+                        .dailyRank(1)
+                        .solvedCount(2)
+                        .maxDifficulty(10)
+                        .problems(Collections.emptyList())
+                        .build())
+                .build();
+        //when: service 메서드 호출시
+        when(myPageService.getMyPageData(anyLong(), eq(year), eq(month), eq(date)))
+                .thenReturn(mockResponse);
+
+        //then : API 호출 및 검증
+        mockMvc.perform(get("/api/user/me")
+                    .param("year", String.valueOf(year))
+                    .param("month", String.valueOf(month))
+                    .param("date", date))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.message").value("마이페이지 조회 성공"))
+                //Profile 검증
+                .andExpect(jsonPath("$.data.profile.username").value("이규동"))
+                .andExpect(jsonPath("$.data.profile.baekjoonId").value("dlrbehd120"))
+                //SelectedDateDetail 검증
+                .andExpect(jsonPath("$.data.selectedDateDetail.date").value(date))
+                .andExpect(jsonPath("$.data.selectedDateDetail.dailyScore").value(20));
+
+
+
+
+
     }
 }
