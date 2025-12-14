b83a9bc339724074e5246348f6476cc0beabc2d5
andrew <leegd120@gmail.com>
2025-12-14 20:00:42 +0900

    fix/#42: CI 에러 해결

--- src/main/java/com/project/common/util/BojUtil.java
+++ src/main/java/com/project/common/util/BojUtil.java
@@ -9,8 +9,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
-import java.util.List;
-import java.util.Map;
 
 import com.project.dto.response.BojUserResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -20,58 +18,58 @@ import org.springframework.stereotype.Component;
 @Component
 public class BojUtil {
 
-  private final Gson gson;
-  private final HttpClient httpClient;
+    private final Gson gson;
+    private final HttpClient httpClient;
 
-  public BojUtil(Gson gson) {
-    this.gson = gson;
+    public BojUtil(Gson gson) {
+        this.gson = gson;
 
-    this.httpClient =
-        HttpClient.newBuilder()
-            .version(HttpClient.Version.HTTP_2)
-            .connectTimeout(Duration.ofSeconds(3)) // 연결 지연 대비
-            .build();
-  }
-
-  public boolean checkBojId(String baekjoonId) {
-    BojUserResponse response = requestBojUser(baekjoonId);
-    return response != null;
-  }
-
-  public int getBojTier(String baekjoonId) {
-    BojUserResponse response = requestBojUser(baekjoonId);
-    return response.tier();
-  }
-
-  private BojUserResponse requestBojUser(String baekjoonId) {
-    HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("https://solved.ac/api/v3/user/show?handle=" + baekjoonId))
-            .header("x-solvedac-language", "ko")
-            .header("Accept", "application/json")
-            .timeout(Duration.ofSeconds(5))
-            .GET()
-            .build();
-
-    try {
-      HttpResponse<String> response =
-              httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-
-      // 존재하지 않는 ID
-      if (response.statusCode() == 404) {
-        return null;
-      }
-
-      if (response.statusCode() != 200) {
-        throw new BusinessException(ErrorCode.BAEKJOON_AUTH_INVALID);
-      }
-
-      return gson.fromJson(response.body(), BojUserResponse.class);
-
-    } catch (InterruptedException e) {
-      Thread.currentThread().interrupt();
-      throw new BusinessException(ErrorCode.BAEKJOON_AUTH_INVALID);
-    } catch (IOException e) {
-      throw new BusinessException(ErrorCode.BAEKJOON_AUTH_INVALID);
+        this.httpClient =
+                HttpClient.newBuilder()
+                        .version(HttpClient.Version.HTTP_2)
+                        .connectTimeout(Duration.ofSeconds(3)) // 연결 지연 대비
+                        .build();
     }
-  }
+
+    public boolean checkBojId(String baekjoonId) {
+        BojUserResponse response = requestBojUser(baekjoonId);
+        return response != null;
+    }
+
+    public int getBojTier(String baekjoonId) {
+        BojUserResponse response = requestBojUser(baekjoonId);
+        return response.tier();
+    }
+
+    private BojUserResponse requestBojUser(String baekjoonId) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://solved.ac/api/v3/user/show?handle=" + baekjoonId))
+                .header("x-solvedac-language", "ko")
+                .header("Accept", "application/json")
+                .timeout(Duration.ofSeconds(5))
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<String> response =
+                    httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            // 존재하지 않는 ID
+            if (response.statusCode() == 404) {
+                return null;
+            }
+
+            if (response.statusCode() != 200) {
+                throw new BusinessException(ErrorCode.BAEKJOON_AUTH_INVALID);
+            }
+
+            return gson.fromJson(response.body(), BojUserResponse.class);
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new BusinessException(ErrorCode.BAEKJOON_AUTH_INVALID);
+        } catch (IOException e) {
+            throw new BusinessException(ErrorCode.BAEKJOON_AUTH_INVALID);
+        }
+    }
 }
