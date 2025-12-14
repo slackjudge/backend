76e37fd3f35ce0b8d174f52ab1a16a64cee3b00f
arkchive <kyungminkim7@konkuk.ac.kr>
2025-12-14 17:42:09 +0900

    refactor/#31: 일일랭킹요약알림, 순위변동 알림 기능 분리

---
+++ src/test/java/com/project/controller/RankChangeStateControllerTest.java
@@ -0,0 +1,43 @@
+package com.project.controller;
+
+import com.project.service.RankChangeStateService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(DailyRankMessageController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@Import(DailyRankMessageControllerTest.TestConfig.class)
+public class RankChangeStateControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    RankChangeStateService rankChangeStateService;
+
+    static class TestConfig {
+        @Bean
+        public RankChangeStateService rankChangeStateService() {
+            return mock(RankChangeStateService.class);
+        }
+    }
+    @Test
+    @DisplayName("개인 순위 변동 알림 API 호출 성공")
+    void rankChangeApiTest() throws Exception {
+
+        mvc.perform(post("/slack/rank-change"))
+                .andExpect(status().isOk());
+
+        verify(rankChangeStateService, times(1)).sendRankChangeMessage();
+    }
+}
