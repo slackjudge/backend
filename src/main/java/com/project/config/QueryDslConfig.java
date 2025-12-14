6bac998387aa278b854e3d4a293ece976fa3f87b
andrew <leegd120@gmail.com>
2025-12-14 19:34:59 +0900

    fix/#42: build 에러 해결

--- src/main/java/com/project/config/QueryDslConfig.java
+++ src/main/java/com/project/config/QueryDslConfig.java
@@ -2,14 +2,19 @@ package com.project.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class QueryDslConfig {
 
-  @Bean
-  public JPAQueryFactory jpaQueryFactory(EntityManager em) {
-    return new JPAQueryFactory(em);
-  }
+    @PersistenceContext
+    private EntityManager em;
+
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
 }
