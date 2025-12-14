e6fd9275efe1ea3b24e929c4ae7bec7d6e81465f
rettooo <gkdud9236@naver.com>
2025-12-15 00:47:44 +0900

    chore/#30-mypage: checkstyle

--- src/main/java/com/project/service/MyPageService.java
+++ src/main/java/com/project/service/MyPageService.java
@@ -16,7 +16,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.DateTimeException;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
