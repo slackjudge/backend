c0ffc3ccb9bc3fa8033efa60d2fdbf9b45230183
arkchive <kyungminkim7@konkuk.ac.kr>
2025-12-16 10:37:03 +0900

    refactor/#51: 로컬 테스트 전용 컨트롤러 분리

--- src/main/java/com/project/controller/RankChangeStateController.java
+++ src/main/java/com/project/controller/RankChangeStateController.java
@@ -3,14 +3,16 @@ package com.project.controller;
 import com.project.common.dto.ApiResponse;
 import com.project.service.RankChangeStateService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Profile("local")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/slack")
+@RequestMapping("/test/slack")
 public class RankChangeStateController {
 
     private final RankChangeStateService rankChangeStateService;
