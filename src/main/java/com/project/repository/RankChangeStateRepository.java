bed2aeed58584e3a163dea7675edfa6ba93908fd
arkchive <kyungminkim7@konkuk.ac.kr>
2025-12-17 23:32:00 +0900

    fix/#68: 불필요한 코드 삭제

--- src/main/java/com/project/repository/RankChangeStateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.repository;
-
-import com.project.entity.RankChangeStateEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RankChangeStateRepository extends JpaRepository<RankChangeStateEntity, Long> {
-}
