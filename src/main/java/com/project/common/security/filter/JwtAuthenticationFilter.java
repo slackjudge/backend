4614ab3f6c6a7a4cc4c216e0dd261c53afa0242e
andrew <leegd120@gmail.com>
2025-12-30 17:20:40 +0900

    feat/#78: 불필요한 의존성 제거

--- src/main/java/com/project/common/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/project/common/security/filter/JwtAuthenticationFilter.java
@@ -10,7 +10,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
